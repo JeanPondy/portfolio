1239a34bd4325535b71a49f187a63ed850e53302
Jean Pondy <manbayesse@gmail.com>
2024-09-21 03:51:05 +0200

    portfolio/title

--- portfoliojp/src/app/app.component.ts
+++ portfoliojp/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component'
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, AboutMeComponent, MySkillsComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
