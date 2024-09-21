8eb9d9d307afbb932b8f4af008609423ac5d0ce6
Jean Pondy <manbayesse@gmail.com>
2024-09-21 02:30:17 +0200

    my-skills-component

--- portfoliojp/src/app/app.component.ts
+++ portfoliojp/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { AboutMeComponent } from './about-me/about-me.component'
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, AboutMeComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
