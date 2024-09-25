1302ed4b7a2307a061e0ae3d7d85c7a73ada33f1
Jean Pondy <manbayesse@gmail.com>
2024-09-25 09:37:31 +0200

    team-feedback updated

--- portfoliojp/src/app/app.routes.ts
+++ portfoliojp/src/app/app.routes.ts
@@ -1,7 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { NgModule } from '@angular/core';
+
+
 
 export const routes: Routes = [
     { path: '', component: ProfileComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
     
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
